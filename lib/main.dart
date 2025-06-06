7418ae0867926cb251b5cb0f897d05b010897013
Murilo-RD <murilo.dhu@gmail.com>
2025-06-06 14:00:28 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:composite_bolsa/telas/tela_principal.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-
+  runApp(
+    MaterialApp(
+      home: TelaPrincipal(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
---
+++ lib/telas/tela_principal.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({super.key});
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              title: Text(
+                "Monte Seu Kit",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              backgroundColor: Color(0xFF4A90E2),
+            )));
+    ;
+  }
+}
